6ae4c11104a72629c61c977a86e1a669c703e4bb
Vogon38 <luana@thecyborg.xyz>
2022-10-10 01:31:18 -0300

    alguns cenários de testes adicionados

---
+++ src/componentes/Formulario.test.tsx
@@ -0,0 +1,67 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { RecoilRoot } from 'recoil';
+import Formulario from './Formulario'
+
+test('quando input está vazio, novos participantes não podem ser adicionados', () => {
+    render(
+        <RecoilRoot>
+            <Formulario />
+        </RecoilRoot>)
+
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+    const botao = screen.getByRole('button')
+
+    expect(input).toBeInTheDocument()
+    expect(botao).toBeDisabled()
+})
+
+test('adicionar um participante caso exista um nome preenchido', () => {
+    render(
+        <RecoilRoot>
+            <Formulario />
+        </RecoilRoot>)
+
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+    const botao = screen.getByRole('button')
+
+    fireEvent.change(input, {
+        target: {
+            value: 'Catarina'
+        }
+    })
+
+    fireEvent.click(botao)
+
+    expect(input).toHaveFocus()
+    expect(input).toHaveValue("")
+})
+
+test('nomes duplicados não podem ser adicionados na lista', () => {
+    render(
+        <RecoilRoot>
+            <Formulario />
+        </RecoilRoot>)
+
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+    const botao = screen.getByRole('button')
+
+    fireEvent.change(input, {
+        target: {
+            value: 'Catarina'
+        }
+    })
+
+    fireEvent.click(botao)
+
+    fireEvent.change(input, {
+        target: {
+            value: 'Catarina'
+        }
+    })
+
+    fireEvent.click(botao)
+    
+    const mensagemDeErro = screen.getByRole('alert')
+
+    expect(mensagemDeErro.textContent).toBe('Nomes duplicados não são permitidos')
+})
